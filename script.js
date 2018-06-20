7445781d2e6b96eb28faf634b0e18c5b9437e445
Alex <w0931500768@gmail.com>
2018-06-20 17:05:55 +0300

    add elements inside body

--- script.js
+++ script.js
@@ -14,4 +14,53 @@ window.addEventListener('load', function(){
 	dateY.innerHTML = date.getFullYear();
 	dateD.innerHTML = dayName[date.getDay()];
 
+
+
+	/*=====CREATE LIST======*/
+
+	const btn = document.querySelector('.btn');
+	const p_noTask = document.querySelector('.task');
+	const bodyList = document.querySelector('.body-list');
+	const form = document.querySelector('form');
+	const list = document.querySelector('.list');
+
+	// CREATE ELEMENTS BY CLICK
+
+	btn.addEventListener('click', function(e){
+		// remove <p>no task...</p>
+		p_noTask.style.display='none';
+		
+		//check for availability addInputs
+		if(form.hasChildNodes(document.querySelector('addInput'))){
+			let children = form.childNodes;
+			for(let i = 0; i < children.length; i++){
+				let saveValue = children[i].value;
+
+				// create tag 'li' for 'ul.list'
+				const task = document.createElement('input');
+
+				// implement new element in 'ul.list'
+				list.appendChild(task);
+
+				// add CSS for new element
+				task.classList.add('item');
+
+			}
+		}
+		
+		// create elements for list		
+		const addInput = document.createElement('input');
+		
+		// implement new elements		
+		form.appendChild(addInput).focus();
+		
+		// add CSS for new elements
+		form.classList.add('form');
+		addInput.classList.add('addInput');
+		
+	});
+
+	// FIX NEW ELEMENTS
+
+
 });
